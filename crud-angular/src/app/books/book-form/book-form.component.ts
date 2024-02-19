2c8d7ba8d5fd19a14ddb5beec372f75c44d6d9b1
João Wojciechowski <joao_woj@outlook.com>
2024-02-19 03:46:30 -0300

    Adicionando validações no front

--- crud-angular/src/app/books/book-form/book-form.component.ts
+++ crud-angular/src/app/books/book-form/book-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { BooksService } from '../services/books.service';
@@ -9,56 +9,95 @@ import { Book } from '../model/book';
 @Component({
   selector: 'app-book-form',
   templateUrl: './book-form.component.html',
-  styleUrl: './book-form.component.css'
+  styleUrl: './book-form.component.css',
 })
 export class BookFormComponent implements OnInit {
-
   form = this.formBuilder.group({
     _id: 0,
-    title: [''],
-    author: [''],
-    isbn: [''],
-    publisher: [''],
+    title: ['', Validators.required, Validators.maxLength(200)],
+    author: ['', Validators.required, Validators.maxLength(200)],
+    isbn: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(20)]],
+    publisher: ['', Validators.required, Validators.maxLength(200)],
     rented: [false],
-    publicationYear: [0]
+    publicationYear: [
+      0,
+      [Validators.required, Validators.min(1000), Validators.max(9999)],
+    ],
   });
 
-  constructor(private formBuilder: NonNullableFormBuilder,
+  constructor(
+    private formBuilder: NonNullableFormBuilder,
     private service: BooksService,
     private snackBar: MatSnackBar,
     private location: Location,
-    private route: ActivatedRoute){
-  };
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     const book: Book = this.route.snapshot.data['book'];
     this.form.setValue({
-      _id:book._id,
-      title:book.title,
+      _id: book._id,
+      title: book.title,
       author: book.author,
       isbn: book.isbn,
       publisher: book.publisher,
       rented: book.rented,
-      publicationYear: book.publicationYear
+      publicationYear: book.publicationYear,
     });
-  };
+  }
 
-  onSubmit(){
-    this.service.save(this.form.value)
-    .subscribe(result => this.onSucess(), error => this.onError())
-  };
+  getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
 
-  onCancel(){
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório';
+    }
+    if (field?.hasError('maxlength')) {
+      const requiredLength = field.errors
+        ? field.errors['maxlength']['requiredLength']
+        : 200;
+      return `Tamanho máximo de ${requiredLength} caracteres excedido.`;
+    }
+
+    if (field?.hasError('min')) {
+      return 'O ano de publicação deve ser a partir do ano 1000.';
+    }
+
+    if (field?.hasError('max')) {
+      return 'O ano de publicação não deve exceder 9999 anos.';
+    }
+
+    return 'Campo inválido';
+  }
+
+  getErrorMessageIsbn() {
+    const field = this.form.get('isbn');
+    if (field?.hasError('maxlength')) {
+      return `Tamanho máximo de 20 caracteres excedido.`;
+    }
+    if (field?.hasError('minlength')) {
+      return `Tamanho mínimo precisa ser de 10 caracteres.`;
+    }
+    return 'Campo inválido';
+  }
+
+  onSubmit() {
+    this.service.save(this.form.value).subscribe(
+      (result) => this.onSucess(),
+      (error) => this.onError()
+    );
+  }
+
+  onCancel() {
     this.location.back();
-  };
+  }
 
-  private onSucess(){
-    this.snackBar.open('Livro salvo com sucesso.', '', {duration: 5000})
+  private onSucess() {
+    this.snackBar.open('Livro salvo com sucesso.', '', { duration: 5000 });
     this.onCancel();
   }
 
-  private onError(){
-    this.snackBar.open('Erro ao salvar livro.', '', {duration: 5000})
+  private onError() {
+    this.snackBar.open('Erro ao salvar livro.', '', { duration: 5000 });
   }
-
 }
